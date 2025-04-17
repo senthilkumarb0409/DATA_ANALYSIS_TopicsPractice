313d7dc7400c7f7f6b41e0dcdc7cf123bc1487a3
SENTHIL KUMAR.B <88877288+senthilkumarb0409@users.noreply.github.com>
2025-04-17 13:05:06 +0530

    17-04-2025 comp ved v22

--- SQL_PRACTICE_github/windowfuctionbasicsv22.sql
+++ SQL_PRACTICE_github/windowfuctionbasicsv22.sql
@@ -25,3 +25,42 @@ from Sales.Orders
 
 --syntax: windowfunction + over clause(partition+ order+ frame)
 
+--find total sales for all orders and total sales for each product,
+--also provide orderid ,order date
+
+select 
+OrderID,OrderDate,ProductID,OrderStatus,
+sum(Sales) over() TotalSales,
+sum(Sales) over(Partition by productID) TotalSalesbyProd,
+sum(sales) over (partition by productID, OrderStatus) totalsalesbypando
+from Sales.Orders
+
+-- rank each order based on their sales from highest to lowest,
+--additionally provide details such orderid & orderdate
+
+select
+OrderID,
+OrderDate,
+Sales,
+rank() over(order by Sales desc) ranksales
+from Sales.Orders
+
+--using frames
+
+select
+OrderID,
+OrderDate,
+OrderStatus,
+Sales,
+sum(Sales) over (partition by OrderStatus Order by OrderDate
+rows between current row and 1 following) totalsales
+from Sales.Orders
+
+--rank customers based on their total sales
+
+select
+CustomerID,
+Sum(Sales) totalsales,
+Rank() over(Order by Sum(Sales) desc ) rankofcusonsales
+from Sales.Orders
+group by CustomerID
